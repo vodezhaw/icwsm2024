b946a759e89398def79c4fb9d5dcf5f9c103b35e
vode <vode@zhaw.ch>
2023-08-23 14:13:54 +0200

    generate latex tables with roc-aucs

---
+++ aaai2023/paper/clf_perf.py
@@ -0,0 +1,95 @@
+
+from pathlib import Path
+
+from jinja2 import Environment, FileSystemLoader
+
+from sklearn.metrics import roc_auc_score
+
+from aaai2023.datasets.hatespeech import load
+from aaai2023.paper.names import (
+    TRAIN_DATASETS,
+    TEST_DATASETS,
+    DATASET_NAMES,
+    CLF_FILE_STEMS,
+)
+
+
+def main(
+    test_folder: Path,
+    scores_folder: Path,
+    out_folder: Path,
+):
+    paired = load(
+        test=test_folder,
+        scores=scores_folder,
+    )
+
+    env = Environment(loader=FileSystemLoader("aaai2023/paper/templates/"))
+    template = env.get_template("perf_table.tex")
+
+    for clf in ["tfidf-svm", "google/electra-base-discriminator", "cardiffnlp/twitter-roberta-base"]:
+        aucs = {
+            DATASET_NAMES[train]: {
+                DATASET_NAMES[test]: 0.
+                for test in TEST_DATASETS
+            }
+            for train in TRAIN_DATASETS
+        }
+
+        rows = [DATASET_NAMES[train] for train in TRAIN_DATASETS]
+        cols = [DATASET_NAMES[test] for test in TEST_DATASETS]
+
+        for (clf_, train, test), b in paired.items():
+            if clf_ != clf:
+                continue
+
+            if train in TRAIN_DATASETS and test in TEST_DATASETS:
+                aucs[DATASET_NAMES[train]][DATASET_NAMES[test]] = roc_auc_score(
+                    y_true=b.labels,
+                    y_score=b.scores,
+                    average=None,
+                    multi_class='raise',
+                )
+
+        tex = template.render(
+            train_datasets=rows,
+            test_datasets=cols,
+            auc=aucs,
+        )
+
+        with (out_folder / f"{CLF_FILE_STEMS[clf]}_rocauc.tex").open('w') as fout:
+            fout.write(f"{tex}\n")
+
+    aucs = {
+        "dummy": {
+            DATASET_NAMES[test]: 0.
+            for test in TEST_DATASETS
+        }
+    }
+    for (clf, train, test), b in paired.items():
+        if clf != "perspective-api":
+            continue
+        assert train == "online-api"
+
+        if test in TEST_DATASETS:
+            aucs["dummy"][DATASET_NAMES[test]] = roc_auc_score(
+                y_true=b.labels,
+                y_score=b.scores,
+                average=None,
+                multi_class="raise",
+            )
+    tex = template.render(
+        train_datasets=["dummy"],
+        test_datasets=cols,
+        auc=aucs,
+    )
+    with (out_folder / "perspective_rocauc.tex").open("w") as fout:
+        fout.write(f'{tex}\n')
+
+
+if __name__ == "__main__":
+    main(
+        test_folder=Path('./data/test'),
+        scores_folder=Path("./data/scores"),
+        out_folder=Path('./artefacts')
+    )
---
+++ aaai2023/paper/names.py
@@ -0,0 +1,63 @@
+
+TEST_DATASETS = [
+    "davidson-hate-off",
+    "davidson-hate-only",
+    "dynamically-generated-hate",
+    "ethos",
+    "ex-machina",
+    "hasoc2019",
+    "hateval2019",
+    "jigsaw",
+    "olid",
+    "solid",
+    "swad",
+    "wassa",
+]
+
+TRAIN_DATASETS = [
+    "davidson-hate-off",
+    "davidson-hate-only",
+    "dynamically-generated-hate",
+    "ex-machina",
+    "hasoc2019",
+    "hateval2019",
+    "jigsaw",
+    "olid",
+    "wassa",
+]
+
+DATASET_NAMES = {
+    "davidson-hate-off": "Davidson Hate & Offensive",
+    "davidson-hate-only": "Davidson Hate Only",
+    "dynamically-generated-hate": "DGH",
+    "ethos": "Ethos",
+    "ex-machina": "Ex Machina",
+    "hasoc2019": "HASOC 2019",
+    "hateval2019": "HatEval 2019",
+    "jigsaw": "Jigsaw",
+    "olid": "OLID",
+    "solid": "SOLID",
+    "swad": "SWAD",
+    "wassa": "WASSA",
+}
+
+CLF = [
+    "tfidf-svm",
+    "google/electra-base-discriminator",
+    "cardiffnlp/twitter-roberta-base",
+    "perspective-api",
+]
+
+CLF_NAMES = {
+    "tfidf-svm": "TF-IDF SVM",
+    "google/electra-base-discriminator": "Electra",
+    "cardiffnlp/twitter-roberta-base": "Twitter-RoBERTa",
+    "perspective-api": "Perspective API",
+}
+
+CLF_FILE_STEMS = {
+    "tfidf-svm": "tfidf-svm",
+    "google/electra-base-discriminator": "electra",
+    "cardiffnlp/twitter-roberta-base": "twitter-roberta",
+    "perspective-api": "perspective",
+}
