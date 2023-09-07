64dd59dc5d74462d639a5e683071117e06b78b66
vode <vode@zhaw.ch>
2023-09-07 23:43:35 +0200

    update subsampling plots for final data

--- aaai2023/paper/subsampling_plots.py
+++ aaai2023/paper/subsampling_plots.py
@@ -2,12 +2,12 @@
 from pathlib import Path
 
 from aaai2023.paper.util import read_results, compute_errors, is_subsampling
-from aaai2023.paper.names import SUBSAMPLING_SUFFIXES, SUBSAMPLING_PREVALENCES
+from aaai2023.paper.names import SUBSAMPLING_PREVALENCES
 from aaai2023.paper.plot_utils import all_box_plots
 
 
 def main():
-    res_path = Path('./data/results.jsonl')
+    res_path = Path('./data/results/prevalence_subsampling.jsonl')
     res = read_results(res_path)
     res = compute_errors(res)
 
@@ -17,18 +17,10 @@ def main():
     ]
 
     # keep only in-domain experiments
-    res = [
-        r
-        for r in res
-        if r.other_domain_scores_file is None
-    ]
+    assert all(r.other_domain_scores_file is None for r in res)
 
     # keep only subsampling experiments
-    res = [
-        r
-        for r in res
-        if is_subsampling(Path(r.scores_file))
-    ]
+    assert all(is_subsampling(Path(r.scores_file)) for r in res)
 
     # ignore errors
     res = [
@@ -36,18 +28,13 @@ def main():
         for r in res
         if r.error_message is None
     ]
-
-    # keep only 50 and 100 samplings
-    res = [
-        r
-        for r in res
-        if r.n_samples_to_select >= 50
-    ]
+    
+    suffixes_to_plot = ["p2", "p3", "p5", "p7", "p10"]
 
     groupings = {}
     for domain in subsampling_domains:
-        for suff in SUBSAMPLING_SUFFIXES:
-            for q_strat in ['CC', "CPCC", "BCC"]:
+        for suff in suffixes_to_plot:
+            for q_strat in ['CC', "PACC", "CPCC", "BCC"]:
                 # jigsaw has no p10
                 if domain == 'jigsaw' and suff == 'p10':
                     continue
@@ -69,13 +56,17 @@ def main():
             x_labels=[
                 f"{SUBSAMPLING_PREVALENCES[suff]:.3f}"
                 for suff in (
-                    SUBSAMPLING_SUFFIXES[:-1] if domain == 'jigsaw' else SUBSAMPLING_SUFFIXES
+                    suffixes_to_plot[:-1] if domain == 'jigsaw' else suffixes_to_plot
                 )
             ],
-            group_labels=['CC', 'CPCC', 'BCC'],
+            group_labels=['CC', "PACC", 'CPCC', 'BCC'],
             group_style={
                 'CC': {
                     'color': 'black',
+                },
+                "PACC": {
+                    "color": "#919999",
+                    "linestyle": "-."
                 },
                 'CPCC': {
                     'color': '#404749',
@@ -86,7 +77,7 @@ def main():
                     'linestyle': '--',
                 },
             },
-            file_name_fn=lambda clf, err: f"./artefacts/subsampling/{domain}-{clf}-{err}.png",
+            file_name_fn=lambda clf, err: f"./paper_plots/subsampling/{domain}-{clf}-{err}.png",
             x_label="Prevalence",
         )
 
