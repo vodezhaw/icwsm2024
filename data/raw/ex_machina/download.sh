61d9d836a0c3d7e8a86079766f4ff4aa026e96b1
vode <vode@zhaw.ch>
2023-09-07 19:46:43 +0200

    raw information for ex-machina data

---
+++ data/raw/ex_machina/download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget "https://ndownloader.figshare.com/files/7554634" -O comments.tsv
+wget "https://ndownloader.figshare.com/files/7554637" -O annotations.tsv
+
---
+++ data/raw/ex_machina/notes.md
@@ -0,0 +1,2 @@
+download url extracted from:
+https://github.com/ewulczyn/wiki-detox/blob/master/src/modeling/get_prod_models.py
