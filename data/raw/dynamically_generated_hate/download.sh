9be0e4b1e4bbf6e80f1f604225d8421215f86c58
vode <vode@zhaw.ch>
2023-09-07 19:44:59 +0200

    add raw dynamically-generated-hate data

---
+++ data/raw/dynamically_generated_hate/download.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -o dynamically_generated_hate.csv "https://raw.githubusercontent.com/bvidgen/Dynamically-Generated-Hate-Speech-Dataset/main/Dynamically%20Generated%20Hate%20Dataset%20v0.2.3.csv"
