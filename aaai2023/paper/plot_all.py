c248abf1d15960deb0903a8a239aaecef9d1ea8f
vode <vode@zhaw.ch>
2023-09-13 03:07:57 +0200

    rename plot script

--- aaai2023/paper/plot_all.py
+++ aaai2023/paper/plot_all.py
@@ -3,7 +3,7 @@ from aaai2023.paper.sampling_fails import main as sampling_fails
 from aaai2023.paper.compare_quantification_methods import main as compare_quant_methods
 from aaai2023.paper.out_of_domain import main as out_of_domain
 from aaai2023.paper.subsampling_plots import main as subsample_prevalence
-from aaai2023.paper.n_sample_plots import main as sample_sizes
+from aaai2023.paper.fewer_samples import main as sample_sizes
 
 
 def main():
