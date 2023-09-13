d3ab94ec7192d2a090f6eba1ab82e26a93470c69
vode <vode@zhaw.ch>
2023-09-13 19:38:12 +0200

    new tables for low prevalence

--- aaai2023/paper/low_prevalence.py
+++ aaai2023/paper/low_prevalence.py
@@ -11,11 +11,6 @@ def main():
     res = read_results(res_path)
     res = compute_errors(res)
 
-    subsampling_domains = [
-        "jigsaw",
-        "ex-machina",
-    ]
-
     # keep only in-domain experiments
     assert all(r.other_domain_scores_file is None for r in res)
 
@@ -32,36 +27,21 @@ def main():
     suffixes_to_plot = ["p2", "p3", "p5", "p7", "p10"]
 
     groupings = {}
-    for domain in subsampling_domains:
-        for suff in suffixes_to_plot:
-            for q_strat in ['CC', "PACC", "CPCC", "BCC"]:
-                # jigsaw has no p10
-                if domain == 'jigsaw' and suff == 'p10':
-                    continue
-                groupings[domain, f"{SUBSAMPLING_PREVALENCES[suff]:.3f}", q_strat] = [
-                    r
-                    for r in res
-                    if r.quant_strategy == q_strat and
-                       suff in r.scores_file and
-                       domain in r.scores_file
-                ]
-
-    table_data = {}
-    for q_strat in ['CC', 'PACC', 'CPCC', 'BCC']:
-        for suff in suffixes_to_plot:
-            table_data[f"{SUBSAMPLING_PREVALENCES[suff]:.3f}", q_strat] = [
+    for suff in suffixes_to_plot:
+        for q_strat in ['CC', "PACC", "CPCC", "BCC"]:
+            groupings[f"{SUBSAMPLING_PREVALENCES[suff]:.3f}", q_strat] = [
                 r
-                for d in subsampling_domains
-                for r in groupings.get((d, f"{SUBSAMPLING_PREVALENCES[suff]:.3f}", q_strat), [])
+                for r in res
+                if r.quant_strategy == q_strat and suff in r.scores_file and r.sample_selection_strategy == 'quantile'
             ]
 
-    for error_type in ["AE"]:
+    for error_type in ["AE", "SAPE"]:
         render_quant_results(
             rows=[f"{SUBSAMPLING_PREVALENCES[suff]:.3f}" for suff in suffixes_to_plot],
             columns=['CC', 'PACC', 'CPCC', 'BCC'],
-            data=table_data,
+            data=groupings,
             error_type=error_type,
-            save_as=f"paper_plots/low_prevalence/selected_{error_type}.tex",
+            save_as=f"paper_plots/low_prevalence/selected_quantile_{error_type}.tex",
         )
 
 
